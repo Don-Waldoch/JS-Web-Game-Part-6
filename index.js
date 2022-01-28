b3b1aa20ceb87f2418cdc549726ed0e920fda0af
unknown <waldoch.don@gmail.com>
2022-01-27 20:14:37 -0600

    Bonus Loop Implemented

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.walkEast()
+// This function will walk the NPC around the board.
+// Optionally use the 'laps' argument to define how
+// many times you want to walk around the board.
+
+function WalkAroundBoard(laps = 1) {
+    if (laps === 0) return;
+    npc.walkNorth(1400)
+       .then(() => npc.walkEast(1200))
+       .then(() => npc.walkSouth(300))
+       .then(() => npc.walkEast(1500))
+       .then(() => npc.walkSouth(1500))
+       .then(() => npc.walkWest(2700))
+       .then(() => npc.walkNorth(400))
+       .then(() => WalkAroundBoard(--laps))
+}
+
+//WalkAroundBoard();// Walk 1 lap  around the board
+WalkAroundBoard(2); // Walk 2 laps around the board
 
 // Create the inventory
 const inventory = newInventory()
